a26a5b6561b4bc90f01ea56feb7550cca895ac1e
Web Design <WebDev@Webs-MacBook-Pro.local>
2018-03-28 22:00:45 -0600

    finish step 1

--- itoa.s
+++ itoa.s
@@ -21,11 +21,11 @@ itoa:
                 add     r4, r5, r5, lsl #2      @ compute new_n*5
                 sub     r4, r1, r4, lsl #1      @ remainder = n - new_n*5*2
                 add     r4, r4, #'0'            @ convert to digit
-                strb    r4, [r0, r2]             @ store in buffer
+                strb    r4, [r0, r2]            @ store in buffer
                 add     r2, r2, #1              @ length++
                 subs    r1, r5, #0              @ n = newn and compare with 0
                 bgt     1b
 
                 @ return number of bytes written
                 mov	r0, r2
-		pop	{r4,r5,r6,pc}
+		        pop	{r4,r5,r6,pc}
